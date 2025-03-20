84819eb6ba76c391b19b0e9b38884e6765deb784
William05112 <liamscervino@gmail.com>
2025-03-20 20:25:54 +0100

    feat: esecizio rivisitato

--- jss/script.js
+++ jss/script.js
@@ -1,20 +1,34 @@
 
-// Ciclo for per numeri crescenti da 1 a 100 e all'interno il ciclo if con le 3 condizioni per stampare Fizz, Buzz o FizzBuzz
+// Versione del programma con stampa unica in console.log
 
 for ( let i = 1; i <= 100; i++){
     let stampa ;
     if ( i % 3 ===0 && i % 5 === 0){
-        stampa = "FizzBuzz";
+        stampa = `${i} FizzBuzz`;
         
     }else if ( i % 3 === 0){
-        stampa = "Fizz"; ;
+        stampa = `${i} Fizz` ;
     }else if ( i % 5 === 0){
-        stampa = "Buzz";
+        stampa = `${i} Buzz`;
 
     }else{
-        stampa = i;
+        stampa = `${i}`;
     }
     console.log(stampa);
+
+// Ciclo for per numeri crescenti da 1 a 100 e all'interno il ciclo if con le 3 condizioni per stampare Fizz, Buzz o FizzBuzz
+    // for ( let i = 1; i <= 100; i++){
+    // if ( i % 3 ===0 && i % 5 === 0){
+    //     console.log(`${i} FizzBuzz`);
+        
+    // }else if ( i % 3 === 0){
+    //     console.log(`${i} Fizz`);
+    // }else if ( i % 5 === 0){
+    //     console.log(`${i} Buzz`);
+
+    // }else{
+    //     console.log(i);
+    // }
     
         
         
